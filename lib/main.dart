5ac30de64c5e4225836948c164a5df70712145f8
Kau <kau@dreamkasper.com>
2022-08-17 18:41:38 +0530

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
@@ -7,6 +8,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final app = await Firebase.initializeApp();
+  print(app.options.appId);
   final appConfig = AppConfig(
     appTitle: 'Kaustubh',
     buildFlavor: 'Production',
